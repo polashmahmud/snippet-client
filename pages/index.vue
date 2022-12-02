2321a5c1e29a2ac3ce188277442743ecd88b1120
Polash Mahmud <polashmahmud@gmail.com>
2022-12-02 22:19:45 +0600

    fresh nuxt install

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1 class="text-3xl font-bold bg-amber-600">
+      Hello world!
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
